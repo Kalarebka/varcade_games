bf5413d1e34a676208aef8754dd7ca2bf208a8e5
Ewelina Sitarczyk <aivellyn8@gmail.com>
2022-04-23 21:19:06 +0200

    Add error handling to game-portal services functions.

--- website/server/game_portal/game_portal/games/services.py
+++ website/server/game_portal/game_portal/games/services.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.http import JsonResponse
 
 import requests
 import logging
@@ -6,15 +7,25 @@ import logging
 
 def get_leaderboard(product_id):
     logging.info(f"Requesting top ten for product: {product_id}")
-    response = requests.get(
+    try:
+        response = requests.get(
         f"{settings.STATS_TRACKER_CONFIG['url']}/" f"leaderboards/top_ten/{product_id}"
-    )
-    return response.json()
+        )
+        return response.json()
+    except requests.exceptions.RequestException as err:
+        logging.error(f"Could not access leaderboard server: {err}")
+        return None   
 
 
 def get_player_stats_for_game(product_id, user_id):
     logging.info(f"Requesting stats for user: {user_id}, product: {product_id}")
-    response = requests.get(
+    try:
+        response = requests.get(
         f"{settings.STATS_TRACKER_CONFIG['url']}/" f"stats/{product_id}/{user_id}"
-    )
-    return response.json()
+        )
+        return response.json()
+    except requests.exceptions.RequestException as err:
+        logging.error(f"Could not access stats server: {err}")
+        return None
+
+# requests.exceptions.RequestException
